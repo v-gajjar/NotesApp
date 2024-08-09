4f2e771dba8a243fb794d3b201b91ba69e626b1f
vgajjar <vgajjar22@gmail.com>
2024-08-09 13:27:28 +0100

    initial commit

---
+++ NotesService/src/main/java/com/app/NotesService/NotesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.NotesService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesServiceApplication.class, args);
+	}
+
+}
---
+++ NotesService/src/test/java/com/app/NotesService/NotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.NotesService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
