f31859797ead2c1b662b900b6131f1b9faebafc8
vgajjar <vgajjar22@gmail.com>
2024-10-01 13:54:14 +0100

    add relevant annotations required to pass the NotesRepository unit test

--- NotesService/src/main/java/com/app/NotesService/model/Note.java
+++ NotesService/src/main/java/com/app/NotesService/model/Note.java
@@ -1,8 +1,17 @@
 package com.app.NotesService.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Note {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String title;
     private String content;
 
