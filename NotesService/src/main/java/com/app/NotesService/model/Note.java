c5ac840737ec828c6d2814f0547bdc5dff362de6
vgajjar <vgajjar22@gmail.com>
2024-10-04 11:53:59 +0100

    replaced GenerationType AUTO with IDENTITY as AUTO was causing Hibernate to append '_seq' to table name

--- NotesService/src/main/java/com/app/NotesService/model/Note.java
+++ NotesService/src/main/java/com/app/NotesService/model/Note.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 public class Note {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
