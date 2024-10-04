05f6fb3bc8e4cc8d060e21726bc25487cafbb0b1
vgajjar <vgajjar22@gmail.com>
2024-10-04 11:30:30 +0100

    add AutoConfigureTestDatabaseAnnotation

--- NotesService/src/test/java/com/app/NotesService/repository/NotesRepositoryTest.java
+++ NotesService/src/test/java/com/app/NotesService/repository/NotesRepositoryTest.java
@@ -4,11 +4,14 @@ import com.app.NotesService.model.Note;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class NotesRepositoryTest {
 
     @Autowired
