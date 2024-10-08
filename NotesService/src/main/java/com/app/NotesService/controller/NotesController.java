653a9f69757f2163f76d4dc1bd517c9d7f0f44c2
vgajjar <vgajjar22@gmail.com>
2024-10-08 14:31:44 +0100

    Add API endpoint to the NotesController for finding a note by id, with most basic implementation to pass the test

--- NotesService/src/main/java/com/app/NotesService/controller/NotesController.java
+++ NotesService/src/main/java/com/app/NotesService/controller/NotesController.java
@@ -6,10 +6,7 @@ import com.app.NotesService.service.NotesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -32,4 +29,12 @@ public class NotesController {
         }
         return new ResponseEntity<Note>( createdNote, HttpStatus.CREATED);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Note> find(@PathVariable Long id){
+        Note foundNote = notesService.findNoteById(id);
+
+        return new ResponseEntity<>( foundNote, HttpStatus.OK);
+    }
 }
+
