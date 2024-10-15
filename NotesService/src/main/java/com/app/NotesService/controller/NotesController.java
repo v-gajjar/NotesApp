fa2c870fceccc1bb06e3665aee6d83d31a3aafb3
vgajjar <vgajjar22@gmail.com>
2024-10-15 17:30:29 +0100

    Add a method annotated with ExceptionHandler in order to return a custom JSON response to the client if the value provided for the note id is non-numerical i.e. 'abc'.

--- NotesService/src/main/java/com/app/NotesService/controller/NotesController.java
+++ NotesService/src/main/java/com/app/NotesService/controller/NotesController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/notes")
 public class NotesController {
@@ -44,6 +47,22 @@ public class NotesController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, exception.getMessage());
         }
         return new ResponseEntity<Note>( note, HttpStatus.OK);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleException(MethodArgumentTypeMismatchException exception) {
+        Map<String, String> errorResponse = new HashMap<>();
+
+        String name = exception.getName();
+
+        String errorMessage = "Incorrect data type provided for " + name;
+        int statusCode = HttpStatus.BAD_REQUEST.value();
+
+        errorResponse.put("message", errorMessage);
+        errorResponse.put("statusCode", Integer.toString(statusCode));
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
 
