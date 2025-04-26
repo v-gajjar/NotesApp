7b14c2ee781f21420c797398839c1cda0f174cc9
vgajjar <vgajjar22@gmail.com>
2025-04-26 17:37:08 +0100

    add a custom method for getting notes by user id

--- NotesService/src/main/java/com/app/NotesService/repository/NotesRepository.java
+++ NotesService/src/main/java/com/app/NotesService/repository/NotesRepository.java
@@ -3,5 +3,8 @@ package com.app.NotesService.repository;
 import com.app.NotesService.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NotesRepository extends JpaRepository<Note, Long> {
+    List<Note> findByUserId(Long userId);
 }
